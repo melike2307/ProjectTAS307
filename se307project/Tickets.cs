6c3affa2b811f193caf55520728db304ddc664c5
melike2307 <melike.ozoglu.173@gmail.com>
2021-06-18 16:40:59 +0300

    Second commit

---
+++ se307project/Tickets.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace se307project
+{
+    class Tickets
+    {
+        private int ticketID;
+        private int tourID;
+        private string status;
+        private double price;
+
+        //Setters and Getters
+        public int TicketID { get => ticketID; set => ticketID = value; }
+        public int TourID { get => tourID; set => tourID = value; }
+        public string Status { get => status; set => status = value; }
+        public double Price { get => price; set => price = value; }
+
+        //Parameterized Constructor
+        public Tickets(int ticketID, int tourID, string status, double price)
+        {
+            TicketID = ticketID;
+            TourID = tourID;
+            Status = status;
+            Price = price;
+        }
+
+        //Non-Parameterized Constructor
+        public Tickets() { }
+    }
+}
---
+++ se307project/Tours.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace se307project
+{
+    class Tours
+    {
+        private int id;
+        private string name;
+        private string startDate;
+        private string finishDate;
+        private string destinationPlace;
+        private string departurePlace;
+        private int tourCapacity;
+        private string tourStatus;
+
+        //Setters and Getters
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string StartDate { get => startDate; set => startDate = value; }
+        public string FinishDate { get => finishDate; set => finishDate = value; }
+        public string DestinationPlace { get => destinationPlace; set => destinationPlace = value; }
+        public string DeparturePlace { get => departurePlace; set => departurePlace = value; }
+        public int TourCapacity { get => tourCapacity; set => tourCapacity = value; }
+        public string TourStatus { get => tourStatus; set => tourStatus = value; }
+
+        //Parameterized Constructor
+        public Tours(string name, int ıd, string startDate, string finishDate, string destinationPlace, string departurePlace, int tourCapacity, string tourStatus)
+        {
+
+            Name = name;
+            Id = ıd;
+            StartDate = startDate;
+            FinishDate = finishDate;
+            DestinationPlace = destinationPlace;
+            DeparturePlace = departurePlace;
+            TourCapacity = tourCapacity;
+            TourStatus = tourStatus;
+        }
+
+        //Non-Parameterized Constructor
+        public Tours() { }
+    }
+}
